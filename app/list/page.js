7c5d204a2f1232d32cb86f9ae96752d14da3698e
uonod1323 <uonod1323@naver.com>
2023-06-23 16:53:26 +0900

    start

--- app/list/page.js
+++ app/list/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   let name = 'park'
   return (
     <div>
-      <h4 className="title" style={{color:'red', fontSize:'30px'}}>홍짱후레쉬</h4>
+      <h4 className="title" style={{color:'red', fontSize:'30px'}}>상품목록</h4>
       <p className="title-sub">{name}</p>
     </div>
   )
